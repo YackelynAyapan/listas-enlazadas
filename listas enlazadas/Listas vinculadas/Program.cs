27f29b7e43d119ceab2935819f6ab3854bd2c2df
YackelynAyapan <yackelynestefany@yahoo.com>
2016-09-09 22:09:28 -0600

    Add files via upload

---
+++ listas enlazadas/Listas vinculadas/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas_vinculadas
+{
+
+   
+        class Node
+        {
+            public Node prev { get; set; }
+            public string name { get; set; }
+            public Node next { get; set; }
+
+            Node head = null;
+            Node last = null;
+
+            
+
+            //insertFirst=("Mario"); insertLast("Main");
+            public void insertFirst(string name)
+            {
+                var node = new Node();
+                node.name = name;
+                if (head != null)
+                {
+                    head.prev = node;
+                    node.next = head;
+                }
+                head = node;
+                if (last == null) 
+                {
+                    last = node;
+                }
+               
+            }
+            public void insertLast(string name)
+            {
+                var node = new Node();
+                node.name = name;
+                if (last != null)
+                {
+                    last.next = node;
+                    node.prev = last;
+                }
+                last = node;
+                if (head == null)
+                {
+                    head = node;
+                }  
+            }
+            //Impresion de los datos 
+            public void imprimir()
+            {
+                Node current = head;
+                while (current != null)
+                {
+                    Console.WriteLine(current.name);
+                    current = current.next;
+                }
+            }
+
+        //Modificar datos 
+            public void Modificar()
+            {
+                Console.WriteLine("Ingrese el nombre que desea remplazar");
+                string primero = Console.ReadLine();
+                Console.WriteLine("Ingrese el nombre para remplazarlo");
+                string segundo = Console.ReadLine();
+                Node current = head;
+
+
+            
+                while (current != null)
+                {
+                    if (current.name == primero)
+                    {
+                        
+                        current.name = segundo;
+                        
+                        break;
+                    }
+                    current = current.next;
+                }
+
+            }
+        //Borrar----
+        public void Borrar()
+        {
+            string delete = Console.ReadLine();
+
+            
+            Node current = head;
+            while (current != null)
+            {
+                if (current.name == delete)
+                {
+                    current.prev.next = current.next;
+                    if (current.next != null)
+                    {
+                        current.next.prev = current.prev;
+                    }
+                    break;
+                }
+                else { }
+                current = current.next;
+            }
+        }
+
+    }
+        
+    }
+     
+
---
+++ listas enlazadas/Listas vinculadas/Programlista.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas_vinculadas
+{
+    class Programlista
+    {
+        static void Main(string[] args)
+        {
+            Node List = new Node();
+            Console.WriteLine("Ingrese 5 datos a la lista");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Inserte el dato {0}", i+1);
+                List.insertFirst(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("La lista quedo asi");
+            List.imprimir();
+
+            
+            Console.WriteLine("\nModifique el dato de la lista");
+            List.Modificar();
+            Console.WriteLine("\nModificados son:");
+            List.imprimir();
+            Console.WriteLine("\nAhora borre el dato de la lista");
+            List.Borrar();
+            Console.WriteLine("\nEl listado ahora queda asi");
+            List.imprimir();
+            Console.WriteLine("\nPresione cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
